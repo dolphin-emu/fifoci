da2235560dea918ac579687701987bd5e9a36dcb
Pierre Bourdon <delroth@gmail.com>
2014-07-10 01:25:08 +0200

    settings: add a default path to the static files directory

--- frontend/fifoci/settings.py
+++ frontend/fifoci/settings.py
@@ -79,6 +79,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Media files (used to store DFFs, PNGs)
