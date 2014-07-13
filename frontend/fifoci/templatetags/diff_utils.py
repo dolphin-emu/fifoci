71542a6c921ad726db4c9dd4fbeeb73f6b444310
Pierre Bourdon <delroth@gmail.com>
2014-07-13 15:20:55 +0200

    frontend diff_utils: rewrite the hashes zipping function to tolerate current being non-existing

--- frontend/fifoci/templatetags/diff_utils.py
+++ frontend/fifoci/templatetags/diff_utils.py
@@ -4,7 +4,9 @@ register = template.Library()
 
 @register.assignment_tag
 def get_zipped_hashes(current, old):
-    if old and old.hashes:
-        return zip(current.hashes_list, old.hashes_list)
-    else:
-        return zip(current.hashes_list, [None] * len(current.hashes_list))
+    current = [] if current is None else current.hashes_list
+    old = [] if old is None else old.hashes_list
+    length = max(len(current), len(old))
+    current += [None] * (length - len(current))
+    old += [None] * (length - len(old))
+    return zip(current, old)
