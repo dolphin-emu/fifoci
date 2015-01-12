5f147139fe650629259a97fcf59ab4b3e29267c9
Pierre Bourdon <delroth@gmail.com>
2015-01-12 05:11:40 +0100

    frontend: switch to postgresql to avoid sqlite3 write slowness

--- frontend/fifoci/settings.py
+++ frontend/fifoci/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'fifoci.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fifoci',
+        'USER': 'fifoci',
     }
 }
 
