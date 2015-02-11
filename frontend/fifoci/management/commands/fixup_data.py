04ca305024de984ec2609276c8d4e241a29c53a5
Pierre Bourdon <delroth@gmail.com>
2015-02-11 04:53:28 +0100

    frontend: add a management command to fixup common data errors

---
+++ frontend/fifoci/management/commands/fixup_data.py
@@ -0,0 +1,34 @@
+# This file is part of the FifoCI project.
+# Copyright (c) 2014 Pierre Bourdon <delroth@dolphin-emu.org>
+# Licensing information: see $REPO_ROOT/LICENSE
+
+from django.core.management.base import BaseCommand, CommandError
+from fifoci.models import FifoTest, Result, Version
+
+class Command(BaseCommand):
+    help = 'Fixes bad data in the database.'
+
+    def handle(self, *args, **options):
+        # Try to reparent changes if we have a hash but no ref.
+        for ver in Version.objects.filter(parent__isnull=True):
+            try:
+                parent = Version.objects.get(hash=ver.parent_hash)
+                print('Fixing %r: parent is %r' % (ver, parent))
+                ver.parent = parent
+                ver.save()
+            except Version.DoesNotExist:
+                continue
+
+        # Look for first_result which are not actually first results.
+        for res in Result.objects.select_related('ver', 'ver__parent').filter(
+                first_result=True, ver__parent__isnull=False):
+            try:
+                prev_res = Result.objects.get(ver=res.ver.parent,
+                                              type=res.type,
+                                              dff=res.dff)
+                print('Fixing %r: previous is %r' % (res, prev_res))
+                res.has_change = res.hashes != prev_res.hashes
+                res.first_result = False
+                res.save()
+            except Result.DoesNotExist:
+                continue
