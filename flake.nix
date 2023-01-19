c3f12364f0ba713ed0a0c5723d7029cc0d7eadd2
Pierre Bourdon <delroth@gmail.com>
2023-01-19 03:30:51 +0100

    flake: initial support for runner

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Continuous integration service running Dolphin FIFO logs to find graphics rendering regressions.";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
+    overlay = nixpkgs.lib.composeManyExtensions [
+      poetry2nix.overlay
+      (final: prev: {
+        fifoci-runner = prev.poetry2nix.mkPoetryApplication {
+          src = prev.poetry2nix.cleanPythonSources { src = ./.; };
+          projectDir = fifoci/runner;
+
+          sourceRoot = "source/fifoci/runner";
+        };
+      })
+    ];
+  } // (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in rec {
+      packages = { inherit (pkgs) fifoci-runner; };
+      defaultPackage = pkgs.fifoci-runner;
+
+      devShells.default = with pkgs; mkShell {
+        buildInputs = [ python3Packages.poetry ];
+      };
+    }
+  ));
+}
