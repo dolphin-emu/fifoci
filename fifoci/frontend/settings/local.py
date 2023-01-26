8730dd535bd090987fe7a9985976180bf7f7e2da
Pierre Bourdon <delroth@gmail.com>
2023-01-26 12:27:55 +0100

    frontend: make django-debug-toolbar work on local runs

--- fifoci/frontend/settings/local.py
+++ fifoci/frontend/settings/local.py
@@ -8,3 +8,10 @@ DATABASES = {
 }
 
 DEBUG = True
+TEMPLATE_DEBUG = True
+
+INSTALLED_APPS = INSTALLED_APPS + ("debug_toolbar",)
+MIDDLEWARE = MIDDLEWARE + ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+
+ALLOWED_HOSTS = ALLOWED_HOSTS + ["localhost", "127.0.0.1"]
+INTERNAL_IPS = ["127.0.0.1"]
--- fifoci/frontend/urls.py
+++ fifoci/frontend/urls.py
@@ -5,6 +5,7 @@
 from django.conf import settings
 from django.conf.urls import include, static, url
 from django.contrib import admin
+from django.urls import path
 
 from . import views
 
@@ -32,3 +33,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
