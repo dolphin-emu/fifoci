3611e305312b1a6616a2c2aa478580c7360ce799
Pierre Bourdon <delroth@gmail.com>
2023-01-26 04:53:43 +0100

    frontend: read production settings from environ

--- fifoci/frontend/settings/production.py
+++ fifoci/frontend/settings/production.py
@@ -1,9 +1,20 @@
+import os
+
 from .base import *
+
+if "SECRET_KEY_FILE" in os.environ:
+    SECRET_KEY = open(os.environ["SECRET_KEY_FILE"]).read()
+
+if "STATIC_ROOT" in os.environ:
+    STATIC_ROOT = os.environ["STATIC_ROOT"]
+
+if "MEDIA_ROOT" in os.environ:
+    MEDIA_ROOT = os.environ["MEDIA_ROOT"]
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "fifoci",
-        "USER": "fifoci",
+        "NAME": os.environ.get("POSTGRES_DB", "fifoci"),
+        "USER": os.environ.get("POSTGRES_USER", "fifoci"),
     }
 }
