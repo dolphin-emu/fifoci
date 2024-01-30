28b63debdf605da809d14ec71c9fc78c1029f833
OatmealDome <julian@oatmealdome.me>
2024-01-30 17:40:33 -0500

    flake: Fix poetry2nix overlay reference

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
     overlay = nixpkgs.lib.composeManyExtensions [
-      poetry2nix.overlay
+      poetry2nix.overlays.default
       (final: prev: {
         fifoci-frontend = prev.poetry2nix.mkPoetryApplication {
           src = prev.poetry2nix.cleanPythonSources { src = ./.; };
