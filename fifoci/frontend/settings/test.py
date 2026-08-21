042900696f653fd483c190402e5278369bf55357
OatmealDome <julian@oatmealdome.me>
2026-08-21 18:12:33 -0400

    frontend: Switch to Django-provided in-memory storage class and drop dj-inmemorystorage

--- fifoci/frontend/settings/test.py
+++ fifoci/frontend/settings/test.py
@@ -2,6 +2,6 @@ from .base import *
 
 SECRET_KEY = "foo"
 
-STORAGES["default"]["BACKEND"] = "inmemorystorage.InMemoryStorage"
+STORAGES["default"]["BACKEND"] = "django.core.files.storage.InMemoryStorage"
 MEDIA_ROOT = "/"
 PNGCRUSH_CMD = None
