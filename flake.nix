9ee810c92a28c15449994a789263264c2dcf8bf1
OatmealDome <julian@oatmealdome.me>
2026-08-21 18:12:33 -0400

    flake: Update nixpkgs to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Continuous integration service running Dolphin FIFO logs to find graphics rendering regressions.";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
   inputs.uv2nix.url = "github:pyproject-nix/uv2nix";
   inputs.uv2nix.inputs.nixpkgs.follows = "nixpkgs";
